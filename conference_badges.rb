7f352294d0c5624da8e722483eba17403b44f054
Amanda Rolon <abrolon87@gmail.com>
2019-12-12 16:11:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 def badge_maker(name)
-  badges = []
+  
   name.each do |name|
-    badges << print "Hello, my name is #{name}"
+     "Hello, my name is #{name}"
   end
-  return badges
+
 end
 
 def batch_badge_creator
